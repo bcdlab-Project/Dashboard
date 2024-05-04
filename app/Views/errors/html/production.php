70c6c2dd26a0803cc1c574f7bc8ca6d632edfba9
Nerexbcd <me@nerexbcd.com>
2024-05-04 18:52:31 +0100

    Fix for production error page

--- app/Views/errors/html/production.php
+++ app/Views/errors/html/production.php
@@ -1,10 +1,10 @@
 <?php
     $data['title'] = lang('Errors.whoops');
-    $data['centerContent'] = true;
+    $data['pageMargin'] = false;
     echo view('templates/header', $data);
 ?>
-    <div class="w-4/5 text-center mx-auto p-8 bg-opacity-50 bg-red-300 rounded-lg">
-        <h1 class="text-5xl font-light mb-1"><?=lang('Errors.whoops')?></h1>
+    <div class="self-center w-4/5 p-8 mx-auto text-center bg-red-300 bg-opacity-50 rounded-lg h-fit">
+        <h1 class="mb-1 text-5xl font-light"><?=lang('Errors.whoops')?></h1>
 
         <p><?=lang('Errors.weHitASnag')?></p>
         <p class="mt-4 underline underline-offset-2"><a href="/">Go Back to Home Page</a></p>
